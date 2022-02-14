c0fa5f7f784769be214e1104a4dbee309a49d096
rkrupnick <rachel@rachelkrupnick.com>
2022-02-14 15:09:50 -0500

    Change get routes to resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :articles
 end
